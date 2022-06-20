8012931f6994f7ebab92eec4a1eaab4c9a543cbd
kyleng <ng.kyle101@gmail.com>
2022-06-20 16:29:12 -0700

    Blob Logic Boilerplate

---
+++ JavaBlobMain/src/Processing/Blob.java
@@ -0,0 +1,49 @@
+package Processing;
+
+import Processing.Util.Distance;
+import Processing.Util.Pixel;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+
+public class Blob {
+    Pixel minCorner, maxCorner, center;
+    Scalar blobHSV;
+    static int distanceThreshold = 10;
+    static int colorThreshold = 35;
+
+    public Blob (Pixel firstPixel){
+        minCorner = firstPixel;
+        maxCorner = firstPixel;
+
+        center = firstPixel;
+
+        blobHSV = firstPixel.colorHSV;
+    }
+
+    public void addPixel(Pixel pixel){
+        minCorner.x = Math.min(minCorner.x, pixel.x);
+        minCorner.y = Math.min(minCorner.y, pixel.y);
+
+        maxCorner.x = Math.max(maxCorner.x, pixel.x);
+        maxCorner.y = Math.max(maxCorner.y, pixel.y);
+
+        center.x = (minCorner.x + maxCorner.x) / 2;
+        center.y = (minCorner.y + maxCorner.y) / 2;
+    }
+
+    // Check if the target pixel is near enough to be considered part of this blob
+    public boolean isNear(Pixel pixel){
+        return Distance.getDistance2D(center, pixel) < distanceThreshold;
+    }
+
+    public boolean isInColorRange(Pixel pixel){
+        return Distance.getColorDistance(blobHSV, pixel.colorHSV) < colorThreshold;
+    }
+
+    public Point getMinCorner(){
+        return new Point(minCorner.x, minCorner.y);
+    }
+    public Point getMaxCorner(){
+        return new Point(maxCorner.x, maxCorner.y);
+    }
+}
