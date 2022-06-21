961ca96b41ec07308e84c398b225975cde3629cb
kyleng <ng.kyle101@gmail.com>
2022-06-20 22:01:02 -0700

    Found Color Return system

---
+++ JavaBlobMain/src/Assets/DebugCode.java
@@ -0,0 +1,31 @@
+package Assets;
+
+import org.opencv.imgproc.Imgproc;
+
+public class DebugCode {
+//    System.out.println("Rows: " + imgSource.rows() + " Cols: " + imgSource.cols());
+//    int x = 211, y = 30;
+//        System.out.println(
+//                "X " + x  + " Y " + y
+//                        + " BGR " + (int)imgSource.get(x,y)[0] + ", " + (int)imgSource.get(x,y)[1] + ", " + (int)imgSource.get(x,y)[2]
+//            + " RGB " + (int)imgSource.get(x,y)[2] + ", " + (int)imgSource.get(x,y)[1] + ", " + (int)imgSource.get(x,y)[0]
+//            );
+//        Imgproc.rectangle(imgSource, new Point(x, y), new Point(x+3, y+3), new Scalar(150,30,150));
+
+//        for (int x = 0; x < imgSource.cols(); x ++){
+//            for (int y = 0; y < imgSource.rows(); y++){
+////                Pixel currentPixel = new Pixel(x,y, imgSource.get(x, y));
+//                System.out.println(
+////                        "X " + currentPixel.x
+////                        + " Y " + currentPixel.y
+//                        "X " + x  + " Y " +y
+//                                + " HSV " + imgSource.get(x,y)[0] + " " + imgSource.get(x,y)[1] + " " + imgSource.get(x,y)[2]
+//                );
+//                //DEBUG
+//
+//                Imgproc.rectangle(imgSource, new Point(x,y), new Point(x,y), new Scalar(150,30,150));
+//                HighGui.waitKey(1);
+//                HighGui.imshow("Source Image", imgSource);
+//            }
+//        }
+}
--- JavaBlobMain/src/Pipeline.java
+++ JavaBlobMain/src/Pipeline.java
@@ -1,9 +1,11 @@
 import Processing.Util.Pixel;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import java.awt.event.MouseEvent;
+import java.util.Scanner;
 
 import static org.opencv.core.Core.NATIVE_LIBRARY_NAME;
 
@@ -14,24 +16,14 @@ public class Pipeline {
         System.out.println("Init OpenCV " + Core.VERSION);
 
         // show image
-        Mat imgSource = Imgcodecs.imread(AssetsFolder + "blueFish.jpg");
-
-        Mat imgHSV = new Mat();
-        Imgproc.cvtColor(imgSource, imgHSV,Imgproc.COLOR_RGB2HSV);
-
-        for (int x = 0; x < imgHSV.rows(); x ++){
-            for (int y = 0; y < imgHSV.cols(); y++){
-                Pixel currentPixel = new Pixel(x,y, imgHSV.get(x, y));
-                System.out.println(
-                        "X " + currentPixel.x
-                        + " Y " + currentPixel.y
-                        + " HSV " + imgHSV.get(x,y)[0] + " " + imgHSV.get(x,y)[1] + " " + imgHSV.get(x,y)[2]
-                );
-            }
-        }
+        Mat imgSource = Imgcodecs.imread(AssetsFolder +
+//                "rgb_image.jpg"
+                "cyan.png"
+        );
 
         HighGui.imshow("Source Image", imgSource);
-        HighGui.imshow("HSV Image", imgHSV);
+
+
         HighGui.waitKey(0);
     }
 }
