852b7894c7bd388267cdb4a428e8c8ae500b9c77
kyleng <ng.kyle101@gmail.com>
2022-06-21 14:54:29 -0700

    Gold Fish Magic Threshold

--- JavaBlobMain/src/Processing/Parameters.java
+++ JavaBlobMain/src/Processing/Parameters.java
@@ -3,10 +3,18 @@ package Processing;
 import org.opencv.core.Scalar;
 
 public class Parameters {
-    public static String AssetsFolder = "/Users/kyleng/IdeaProjects/BlobDetection/JavaBlobMain/src/Assets/",
-    ImageName = "blueFish.jpg";
 
-    public static Scalar TargetRGB = new Scalar(43, 143, 253);
+    // blueFish
+//    public static String AssetsFolder = "/Users/kyleng/IdeaProjects/BlobDetection/JavaBlobMain/src/Assets/",
+//    ImageName = "blueFish.jpg";
+//    public static Scalar TargetRGB = new Scalar(43, 143, 253);
+//    public static int DistanceThreshold = 290;
+//    public static int ColorThreshold = 150;
+
+    // GoldFish
+    public static String AssetsFolder = "/Users/kyleng/IdeaProjects/BlobDetection/JavaBlobMain/src/Assets/",
+            ImageName = "GoldFish.png";
+    public static Scalar TargetRGB = new Scalar(220, 72, 29);
     public static int DistanceThreshold = 290;
-    public static int ColorThreshold = 150;
+    public static int ColorThreshold = 100;
 }
