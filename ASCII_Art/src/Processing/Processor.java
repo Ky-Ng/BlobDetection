6c21f59e86970c39348feebfde4d557f0149b4b3
kyleng <ng.kyle101@gmail.com>
2022-06-23 16:10:30 -0700

    Formatting

--- ASCII_Art/src/Processing/Processor.java
+++ ASCII_Art/src/Processing/Processor.java
@@ -23,8 +23,7 @@ public class Processor {
         String ret = "";
         for (int i = 0; i < gray.rows(); i+=Parameters.Decimation){
             for (int j = 0; j < gray.cols(); j+=Parameters.Decimation){
-                ret += reverseMapColorToASCII(gray.get(i, j)[0]); // see OpenCVDocumentation.md
-//                System.out.print(ret);
+                ret += reverseMapColorToASCII(gray.get(i, j)[0]);
             }
             ret += "\n";
         }
@@ -34,15 +33,20 @@ public class Processor {
     public String mapColorToASCII(double grayShade){
         double darkness = grayShade / 255.0;
         int charIndex = (int) (Parameters.ASCII_ColorScale.length() * darkness);
+
         // if the charIndex is the very last (gray = 255 and darkness = 1.0) substring will return "" not " "
         charIndex = charIndex >= Parameters.ASCII_ColorScale.length() ? Parameters.ASCII_ColorScale.length() - 1 : charIndex;
+
         return Parameters.ASCII_ColorScale.substring(charIndex, charIndex+1);
     }
+
     public String reverseMapColorToASCII(double grayShade){
         double darkness = grayShade / 255.0;
         int charIndex = (int) (Parameters.ASCII_ColorScale.length() * darkness);
+
         // if the charIndex is the very first (black = 0 and darkness = 0), black needs to be " " substring will return "" not " "
         charIndex = charIndex <= 0 ? 1 : charIndex;
+
         return Parameters.ASCII_ColorScale.substring(
                 Parameters.ASCII_ColorScale.length() - charIndex,
                 Parameters.ASCII_ColorScale.length() - charIndex + 1
