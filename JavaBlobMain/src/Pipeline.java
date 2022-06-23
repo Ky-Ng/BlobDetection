206ee5a38c1643c478c91255ab887fd645f742cd
kyleng <ng.kyle101@gmail.com>
2022-06-22 22:38:38 -0700

    Add Demo Video like animations

--- JavaBlobMain/src/Pipeline.java
+++ JavaBlobMain/src/Pipeline.java
@@ -9,6 +9,7 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static org.opencv.core.Core.NATIVE_LIBRARY_NAME;
 
@@ -23,8 +24,18 @@ public class Pipeline {
         Mat prePrune = imgSource.clone();
         Mat postPrune = imgSource.clone();
 
+        Point textDisplayPoint = new Point(10,440);
+        Scalar textColor = new Scalar(0,0,0);
+        int font = Imgproc.FONT_HERSHEY_PLAIN;
+        double fontSize = 3.0;
+        int fontThickness = 3;
+
         // process image
         ArrayList<Blob> blobs = new ArrayList<>();
+
+        Imgproc.putText(blobProcess, "1. Find Blobs", textDisplayPoint, font, fontSize, textColor,fontThickness);
+        HighGui.imshow("Demo", blobProcess);
+        HighGui.waitKey(1500);
 
         for (int x = 0; x < imgSource.cols(); x += Parameters.Decimation) {
             for (int y = 0; y < imgSource.rows(); y+= Parameters.Decimation) {
@@ -51,29 +62,42 @@ public class Pipeline {
                         }
                     }
                 }
-                Imgproc.rectangle(blobProcess, new Point(x, y), new Point(x+3, y+3), new Scalar(150,30,150));
+//                Imgproc.rectangle(blobProcess, new Point(x, y), new Point(x+3, y+3), new Scalar(150,30,150), 6);
                 Drawer.drawBlobs(blobProcess, blobs);
-
-
+                HighGui.imshow("Demo", blobProcess);
+                HighGui.waitKey(15);
             }
         }
 
-        //display image
+        // reset display to source
+        HighGui.imshow("Demo", imgSource);
+        HighGui.waitKey(300);
+
+        // show the blobs created
+
+        Imgproc.putText(prePrune, "2. Merge/Prune Blobs", textDisplayPoint, font, fontSize, textColor,fontThickness);
+        HighGui.imshow("Demo", prePrune);
+        HighGui.waitKey(1700);
         Drawer.drawBlobs(prePrune, blobs);
+
+        HighGui.imshow("Demo", prePrune);
+        HighGui.waitKey(1700);
+
+        // show pruned blobs/final
+        Imgproc.putText(imgSource, "3. Final Bounding Boxes", textDisplayPoint, font, fontSize, textColor,fontThickness);
+        HighGui.imshow("Demo", imgSource);
+        HighGui.waitKey(900);
 
         BlobsUtil.pruneBlobs(blobs);
         Drawer.drawBlobs(postPrune, blobs);
 
-        BlobsUtil.printDistance(blobs);
-        BlobsUtil.printArea(blobs);
 
-        HighGui.imshow("1 Source", imgSource);
-        HighGui.imshow("2 Processing", blobProcess);
-        HighGui.imshow("3 Pre Prune", prePrune);
-        HighGui.imshow("4 Post Prune", postPrune);
-        for (Blob b : blobs){
-            System.out.println(b.getMinCorner() +""+ b.getMaxCorner());
-        }
+
+        Imgproc.putText(postPrune, "3. Final Bounding Boxes", textDisplayPoint, font, fontSize, textColor,fontThickness);
+        HighGui.imshow("Demo", postPrune);
+        HighGui.waitKey(1700);
+
+        HighGui.imshow("Demo", postPrune);
         HighGui.waitKey(0);
     }
 }
